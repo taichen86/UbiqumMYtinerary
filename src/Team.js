062430aa3a4eed189c7fd7783ba3c0e2157e59b7
Victoria Chen <taichen86@gmail.com>
2019-05-21 14:47:29 +0200

    team class

--- src/Team.js
+++ src/Team.js
@@ -4,16 +4,19 @@ import TeamMember from './TeamMember';
 const MEMBERS = [{ name: "Marting Wright", avatarURL: "avatarMartin.png" },
                 { name: "Sabrina Miller", avatarURL: "avatarSabrina.png"}];
 
-function Team( ){
-    return (
-        <div>
-            <h2>MYtinerary Project Team</h2>
-            {MEMBERS.map( member => {
-               return <TeamMember name={member.name} avatarURL={member.avatarURL}></TeamMember>;
-            })};
+class Team extends React.Component {
+    render(){
+        return (
+            <div>
+                <h2>MYtinerary Project Team</h2>
+                {MEMBERS.map( member => {
+                   return <TeamMember name={member.name} avatarURL={member.avatarURL}></TeamMember>;
+                })};
+    
+            </div>
+        );
+    }
 
-        </div>
-    );
 }
 
 export default Team;
