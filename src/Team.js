14768e30fdf0ac86a459e4ec5d21be8256e42f40
Victoria Chen <taichen86@gmail.com>
2019-05-21 15:19:43 +0200

    click handler

--- src/Team.js
+++ src/Team.js
@@ -4,15 +4,19 @@ import TeamMember from './TeamMember';
 const MEMBERS = [{ name: "Marting Wright", avatarURL: "avatarMartin.png" },
                 { name: "Sabrina Miller", avatarURL: "avatarSabrina.png"}];
 
+const Content = () => ( <div></div> );
+
 class Team extends React.Component {
+
+   
+
     render(){
         return (
             <div>
                 <h2>MYtinerary Project Team</h2>
                 {MEMBERS.map( member => {
-                   return <TeamMember name={member.name} avatarURL={member.avatarURL}></TeamMember>;
+                return <TeamMember name={member.name} avatarURL={member.avatarURL}></TeamMember>;
                 })};
-    
             </div>
         );
     }
--- src/TeamMember.js
+++ src/TeamMember.js
@@ -1,12 +1,32 @@
 import React from 'react'
 
 
-function TeamMember( props ){
-    return(
-        <div>
-            <h5>Team Member's Name: {props.name}</h5>
-            <img src={props.avatarURL} />
-        </div>
-    );
+class TeamMember extends React.Component {
+
+    constructor( props ){
+        super( props );
+        this.state = { isVisible: true };
+        this.toggleVisibility = this.toggleVisibility.bind( this );
+    }
+
+    toggleVisibility(){
+        console.log( "clicked ");
+        this.setState( state => ({
+            isVisible : !state.isVisible
+        }));
+    }
+
+    render(){
+        return(
+            <div>
+                <button onClick={this.toggleVisibility}>
+                Toggle Visibility
+                </button>
+                <h5>Team Member's Name: {this.props.name}</h5>
+                <img src={this.props.avatarURL} />
+            </div>
+        );
+    }
+
 }
 export default TeamMember;
